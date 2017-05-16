e95b90bd41fe51d7bada477c87141cb2a4fc60f7
Bill <iamkg05@gmail.com>
2017-05-15 21:20:20 -0500

    new change

--- ptt_crawler.py
+++ ptt_crawler.py
@@ -7,19 +7,21 @@ import io
 import threading
 import os
 import random
+import codecs
 from time import sleep
 from datetime import datetime
 from bs4 import BeautifulSoup  
+from six import u
 requests.packages.urllib3.disable_warnings()
 
-PTT_board=""
-load={
-'from':'/bbs/'+PTT_board+'/index.html',
-'yes':'yes' 
-}
+# PTT_board=""
+# load={
+# 'from':'/bbs/'+PTT_board+'/index.html',
+# 'yes':'yes' 
+# }
 
 rs=requests.session()
-res=rs.post('https://www.ptt.cc/ask/over18',verify=False,data=load)
+# res=rs.post('https://www.ptt.cc/ask/over18',verify=False,data=load)
 
 def PageCount(PTT_board):
     res=rs.get('https://www.ptt.cc/bbs/'+PTT_board+'/index.html',verify=False)
@@ -28,48 +30,104 @@ def PageCount(PTT_board):
     ALLpage=int(getPageNumber(ALLpageURL))+1
     return  ALLpage 
 
-def parseGos(link , g_id, data):
+def parseGos(link , data_to_store):
     print link
-    res=rs.get(link,verify=False)
-    soup = BeautifulSoup(res.text,'html.parser')
-    # author
-    author  = soup.select('.article-meta-value')[0].text
-    # title
-    title = soup.select('.article-meta-value')[2].text
-    # date
-    date = soup.select('.article-meta-value')[3].text
-    # content
-    content = soup.find(id="main-content").text
-    target_content=u'※ 發信站: 批踢踢實業坊(ptt.cc),'
-    content = content.split(target_content)
-    content = content[0].split(date)
-    main_content = content[1].replace('\n', '  ').replace('\t', '  ')
-    
-    # message
-    num, message = 0, [] 
-    for tag in soup.select('div.push'):
-        num += 1
-        push_userid = tag.find("span", {'class': 'push-userid'}).text       
-        push_content = tag.find("span", {'class': 'push-content'}).text   
-        push_content = push_content[1:]
-        message.append(push_userid.encode('utf-8')+":"+push_content.encode('utf-8'))
-    d={
-       "ID":g_id ,
-       "date":date.encode('utf-8'),
-       "title":title.encode('utf-8'),
-       "author":author.encode('utf-8'),
-       "content":main_content.encode('utf-8'),
-       "message":" ".join(message),
-       "link":str(link)
-       }
-    json_data = json.dumps(d,ensure_ascii=False,sort_keys=True)+','
-    data.append(json_data)
+    resp = requests.get(url=link, cookies={'over18': '1'}, verify=False)
+    if resp.status_code != 200:
+        print('invalid url:', resp.url)
+        return json.dumps({"error": "invalid url"}, sort_keys=True, ensure_ascii=False)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    main_content = soup.find(id="main-content")
+    metas = main_content.select('div.article-metaline')
+    author = ''
+    title = ''
+    date = ''
+    if metas:
+        author = metas[0].select('span.article-meta-value')[0].string if metas[0].select('span.article-meta-value')[0] else author
+        title = metas[1].select('span.article-meta-value')[0].string if metas[1].select('span.article-meta-value')[0] else title
+        date = metas[2].select('span.article-meta-value')[0].string if metas[2].select('span.article-meta-value')[0] else date
+
+        # remove meta nodes
+        for meta in metas:
+            meta.extract()
+        for meta in main_content.select('div.article-metaline-right'):
+            meta.extract()
+
+    # remove and keep push nodes
+    pushes = main_content.find_all('div', class_='push')
+    for push in pushes:
+        push.extract()
+
+    try:
+        ip = main_content.find(text=re.compile(u'※ 發信站:'))
+        ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
+    except:
+        ip = ""
+
+    filtered = [ v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--'] ]
+    expr = re.compile(u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
+    for i in range(len(filtered)):
+        filtered[i] = re.sub(expr, '', filtered[i])
+
+    filtered = [_f for _f in filtered if _f]  # remove empty strings
+    content = ' '.join(filtered)
+    content = re.sub(r'(\s)+', ' ', content)
+    messages = []
+    for push in pushes:
+        if not push.find('span', 'push-tag'):
+            continue
+        push_userid = push.find('span', 'push-userid').string.strip(' \t\n\r')
+        # if find is None: find().strings -> list -> ' '.join; else the current way
+        push_content = push.find('span', 'push-content').strings
+        push_content = ' '.join(push_content)[1:].strip(' \t\n\r')  # remove ':'
+        push_ipdatetime = push.find('span', 'push-ipdatetime').string.strip(' \t\n\r')
+        messages.append(push_userid + ":" + push_content)
+
+    data = {
+        'title': title,
+        'link': link,
+        'author': author,
+        'date': date,
+        'content': content,
+        'ip': ip,
+        'messages': messages
+    }
+    data_to_store.append(json.dumps(data, sort_keys=False, ensure_ascii=False)+",")
+
+def crawler(PTT_board, begin, end, thread_number, data):
+    for number in range(begin, end, -1):
+        _url = 'https://www.ptt.cc/bbs/'+PTT_board+'/index'+str(number)+'.html'
+        res=rs.get(_url,verify=False)
+        soup = BeautifulSoup(res.text,'html.parser')
+        data = []
+        for tag in soup.select('div.title'):
+            try:
+                atag=tag.find('a')
+                time=random.uniform(1, 10)/5
+                sleep(time)
+                if(atag):
+                    URL=atag['href'].strip()   
+                    link='https://www.ptt.cc'+URL
+                    parseGos(link, data)                     
+            except Exception, err:
+                print '\033[91m'+ err + '\033[0m'
+        store(data, thread_number, PTT_board)
+
+def store(data, thread_number, PTT_board):
+    PTT_board = PTT_board.lower()
+    print '\033[91m' + "Storing Thread-" + thread_number + '\033[0m'
+    if not os.path.exists(PTT_board):
+        os.makedirs(PTT_board)
+    FILENAME = PTT_board + '/data-' + thread_number + '.json'
+    with codecs.open(FILENAME, 'a', encoding='utf-8') as f:
+        if os.stat(FILENAME).st_size == 0:
+            f.write("[")
+        f.write("\n".join(data))
 
 def remove(value, deletechars):
     for c in deletechars:
         value = value.replace(c,'')
     return value.rstrip();
-   
 
 def getPageNumber(content) :
     startIndex = content.find('index')
@@ -87,37 +145,6 @@ def groupby(n_list):
     grouped_list.append([grouped_list[-1][-1], 0])
     return grouped_list
 
-def crawler(PTT_board, begin, end, threadname, g_id, data):
-    for number in range(begin, end, -1):
-        seen = {}
-        _url = 'https://www.ptt.cc/bbs/'+PTT_board+'/index'+str(number)+'.html'
-        res=rs.get(_url,verify=False)
-        soup = BeautifulSoup(res.text,'html.parser')
-        data = []
-        for tag in soup.select('div.title'):
-            try:
-                atag=tag.find('a')
-                time=random.uniform(1, 5)/5
-                sleep(time)
-                if(atag):
-                    URL=atag['href'].strip()   
-                    link='https://www.ptt.cc'+URL
-                g_id = g_id+1
-                parseGos(link, g_id, data)                     
-            except:
-                pass
-        store(data, threadname, PTT_board)
-
-def store(data, threadname, PTT_board):
-    PTT_board = PTT_board.lower()
-    if not os.path.exists(PTT_board):
-        os.makedirs(PTT_board)
-    FILENAME = PTT_board + '/data-' + threadname + '.json'
-    with open(FILENAME, 'a') as f:
-        if os.stat(FILENAME).st_size == 0:
-            f.write("[")
-        f.write("\n".join(data))
-
 def addBrackets(PTT_board):
     cwd = os.getcwd()
     for _file in os.listdir(cwd + '/' + PTT_board):
@@ -132,21 +159,20 @@ def addBrackets(PTT_board):
             myFile.write("]") 
 
 class myThread(threading.Thread):
-    def __init__(self, PTT_board, begin, end, threadname):
+    def __init__(self, PTT_board, begin, end, thread_number):
         threading.Thread.__init__(self)
         self.PTT_board = PTT_board
         self.begin = begin
         self.end = end
-        self.threadname = threadname
+        self.thread_number = thread_number
         self.data = list()
-        self.g_id = 0
     def run(self):
-        crawler(self.PTT_board, self.begin, self.end, self.threadname, self.g_id, self.data)
+        crawler(self.PTT_board, self.begin, self.end, self.thread_number, self.data)
 if __name__ == "__main__":  
     PTT_board = str(sys.argv[1]) 
     print 'Start parsing [',PTT_board,']....'
     all_page = PageCount(PTT_board)
-    divide_pages = [x for x in range(all_page, 0, -all_page/150)]
+    divide_pages = [x for x in range(all_page, 0, -all_page/100)]
     divide_pages_grouped = groupby(divide_pages)
     print divide_pages_grouped
 
@@ -157,11 +183,11 @@ if __name__ == "__main__":
       thread = myThread(PTT_board, divide_pages[0], divide_pages[1], str(i))
       threads.append(thread)
     for x in threads:
-        print x.threadname + " Starting"
+        print "Thread-" + x.thread_number + " Starting"
         x.start()
     for x in threads:
         x.join()
-        print x.threadname + " Finished"   
+        print "Thread-" + x.thread_number + " Finished"
     addBrackets(PTT_board) 
     print "Exited Thread"
 
